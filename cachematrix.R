f8e8071b7bcea43c274c40f643ec448476802721
benirvin <benirvin@comcast.net>
2018-02-25 23:34:33 -0500

    Expanding the initial comment explaining what these functions do

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions illustrate use of an object model in R to avoid
+## unnecessary repeated calculations.  Given an invertible matrix,
+## if the inverse of that matrix is needed more than once in a program
+## these functions will save the inverse and return the saved value
+## on subsequent calls.  First once must pass the matrix to the 
+## makeCacheMatrix function, assigning the result to a new variable.
+## This variable will formally be a list of functions to set or get both
+## the numeric matrix or its inverse.  The cacheSolve function is then
+## used to obtain the inverse and will return the cached result when it 
+## is available or will apply the solve() function to find the inverse 
+## the first time it is called on a given instance of the object it creates
 
 ## this one creates an object defining get & set for the data and for 
 ## the resulting matrix inverse and containing the original data matrix
