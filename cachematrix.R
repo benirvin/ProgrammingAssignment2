e217e4ab7c30c325fd493d1fae7655ba2d0a3e25
benirvin <benirvin@comcast.net>
2018-02-25 23:20:04 -0500

    This is my solution to the assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this one creates an object defining get & set for the data and for 
+## the resulting matrix inverse and containing the original data matrix
+## in its environment
 
 makeCacheMatrix <- function(x = matrix()) {
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) im <<- inverse
+    getinv <- function() im
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This one takes on object as jsut defined as argument and either
+## 1) computes and returns the matrix inverse, or
+## 2) retrunrs the cached inverse if it exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    im <- x$getinv()
+    if(!is.null(im)) {
+        message("getting cached data")
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setinv(im)
+    im
 }
